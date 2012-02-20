6d7b84432690ab24fce9331e99c88a47c58ba6fc
juacompe <juacompe@gmail.com>
2012-02-20 19:06:14 +0700

    reset_db works runserver works (comes with admin)

--- protem/project_name_project/settings.py
+++ protem/project_name_project/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = '{{ project_name }}.urls'
+ROOT_URLCONF = '{{ project_name }}_project.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = '{{ project_name }}_project.wsgi.application'
