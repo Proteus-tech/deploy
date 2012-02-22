ac1185008ec8eb98d92ad380d0217609501559f2
juacompe <juacompe@gmail.com>
2012-02-23 00:53:29 +0700

    - remove project_template/__init__.py - update setup.py so that the template is included in the package

--- setup.py
+++ setup.py
@@ -2,9 +2,23 @@ import os
 from setuptools import setup
 
 setup(
-    name = "proteus-deploy",
-    version = "0.0.1",
-    author = "Proteus Technologies",
-    author_email = "dev-team@proteus-tech.com",
-    scripts = ["project_template/bin/startproject"],
+    name = 'proteus-deploy',
+    version = '0.0.1',
+    author = 'Proteus Technologies',
+    author_email = 'dev-team@proteus-tech.com',
+    scripts = ['project_template/bin/startproject'],
+    packages = ['project_template'
+        , 'project_template.project_name'
+        , 'project_template.project_name.tests'
+        , 'project_template.project_name_project'
+        , 'project_template.project_name_project.settings'
+    ],
+    package_data = {
+        'project_template': ['setup/*'
+            ,'buildbot/*'
+            ,'runtests'
+            ,'runserver'
+            ,'reset_db' 
+        ],
+    }
 )
