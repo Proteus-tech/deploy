928af306734a99d7f8fa19a1d3fb29ea3e85783a
juacompe <juacompe@gmail.com>
2012-02-28 13:30:05 +0700

    - add a test for create_python_package - refactored create_python_package and pythonify into another script (for testing) - working on pythonify

---
+++ project_template/bin/startproject.py
@@ -0,0 +1,24 @@
+from subprocess import call
+import os, shutil
+
+def create_python_package(path):
+    os.makedirs(path)
+    call(["touch", os.path.join(path, '__init__.py')])
+
+def pythonify(template_path, file_name, src_dir, dst_dir):
+    """
+    Try to fool django that this is a python file by moving it
+    into a python library.
+    Parameters:
+    - file_name
+    - src_dir*, the folder that contains the file
+    - dst_dir*, the destination folder 
+    
+    * src_dir and dst_dir are collection of folder name to os.path.join
+    """
+    src_dir = src_dir + [file_name]
+    dst_dir = dst_dir + [file_name + '.py']
+    src = os.path.join(template_path, *src_dir)
+    dst = os.path.join(template_path, *dst_dir)
+    shutil.move(src, dst)
+
--- tests/test_project_template.py
+++ tests/test_project_template.py
@@ -1,6 +1,35 @@
 from unittest import TestCase
 from project_template.project_name_project import settings
+from project_template.bin.startproject import create_python_package, pythonify
+import os, shutil
 
+class TestStartProjectScript(TestCase):
+    """
+    bin/startproject
+    """
+    def tearDown(self):
+        if os.path.exists('teddy'):
+            shutil.rmtree('teddy')
+
+    def test_create_python_package(self):
+        # Act
+        create_python_package('teddy') 
+        # Assert
+        self.assertTrue(os.path.exists('teddy'))
+        self.assertTrue(os.path.exists('teddy/__init__.py'))
+
+    def test_pythonify(self):
+        # Arrange
+        shutil.copyfile('tests/sample_file_to_pythonify', 
+                        'tests/sample_file_to_pythonify.tmp')
+        self.test_create_python_package() # create teddy
+        template_path = '.'
+        file_name = 'sample_file_to_pythonify.tmp'
+        source = ['tests']
+        destination = ['teddy']
+        pythonify(template_path, file_name, source, destination)
+        
+        
 class TestTransactionMiddleWare(TestCase):
     def test_transaction_middleware_exist(self):
         """
@@ -9,8 +38,6 @@ class TestTransactionMiddleWare(TestCase):
         We should use transaction middleware by default to avoid dealing
         with transaction problem later on when a project needs it.
         """
-        print settings.MIDDLEWARE_CLASSES
-        print settings.ROOT_URLCONF
         self.assertTrue('django.middleware.transaction.TransactionMiddleware' in
-                      settings.MIDDLEWARE_CLASSES)
+                        settings.MIDDLEWARE_CLASSES)
         
