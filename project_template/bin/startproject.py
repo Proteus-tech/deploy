9a14b6d21b5069baeb7aa689ddd3b0d4bb00f837
juacompe <juacompe@gmail.com>
2012-02-28 15:09:20 +0700

    - add test_unpythonify, test_create_project, test_cleanup,   test_chmod_scripts
    - refactor create_project, unpythonify, cleanup, chmod_scripts
      into startproject.py

--- project_template/bin/startproject.py
+++ project_template/bin/startproject.py
@@ -1,5 +1,11 @@
+from django.core import management
 from subprocess import call
 import os, shutil
+
+def create_project(project_name, template_path):
+    template_option = "--template=%s" % template_path
+    management.execute_from_command_line(["django-admin.py", "startproject", project_name, template_option])
+    
 
 def create_python_package(path):
     os.makedirs(path)
@@ -23,4 +29,26 @@ def pythonify(template_path, file_name, src_dir, dst_dir):
     src = os.path.join(template_path, *src_dir)
     dst = os.path.join(template_path, *dst_dir)
     shutil.move(src, dst)
+
+def unpythonify(project_dir, file_name, src_dir, dst_dir):
+    src_dir = src_dir + [file_name + '.py']
+    dst_dir = dst_dir + [file_name]
+    src = os.path.join(project_dir, *src_dir)
+    dst = os.path.join(project_dir, *dst_dir)
+    shutil.move(src, dst)
+ 
+def cleanup(project_dir):
+    """
+    remove build folder and __init__.py in project_dir
+    """
+    build_root = os.path.join(project_dir, 'build')
+    shutil.rmtree(build_root)
+    project_init = os.path.join(project_dir, '__init__.py')
+    os.remove(project_init)
+
+def chmod_scripts(project_dir):
+    scripts = ['runtests', 'runserver', 'reset_db']
+    for script in scripts:
+        script_path = os.path.join(project_dir, script)
+        os.chmod(script_path, 0744)
 
--- tests/test_project_template.py
+++ tests/test_project_template.py
@@ -1,7 +1,12 @@
-from unittest import TestCase
 from project_template.project_name_project import settings
-from project_template.bin.startproject import create_python_package, pythonify
-import os, shutil
+from project_template.bin.startproject import (create_python_package, pythonify,
+    unpythonify, create_project, cleanup, chmod_scripts)
+
+from subprocess import call
+
+from unittest import TestCase
+
+import os, shutil, stat
 
 class TestStartProjectScript(TestCase):
     """
@@ -10,6 +15,12 @@ class TestStartProjectScript(TestCase):
     def tearDown(self):
         if os.path.exists('build'):
             shutil.rmtree('build')
+        if os.path.exists('teddy'):
+            shutil.rmtree('teddy')
+        if os.path.exists('sample_project'):
+            shutil.rmtree('sample_project')
+        if os.path.exists('tests/sample_file_to_pythonify.tmp'):
+            os.remove('tests/sample_file_to_pythonify.tmp')
 
     def test_create_python_package(self):
         # Act
@@ -19,6 +30,16 @@ class TestStartProjectScript(TestCase):
         self.assertTrue(os.path.exists('build/__init__.py'))
 
     def test_pythonify(self):
+        """
+        Scenario: 
+        1. clone tests/sample_file_to_pythonify (avoid the test screwing it up)
+        2. create build package
+        3. pythonify the cloned file into the package
+        Expected:
+        - the cloned file is moved into the package
+        - .py is appended in the cloned file's name
+        - the content in the file still ok
+        """
         # Arrange
         shutil.copyfile('tests/sample_file_to_pythonify', 
                         'tests/sample_file_to_pythonify.tmp')
@@ -30,11 +51,71 @@ class TestStartProjectScript(TestCase):
         # Act
         pythonify(template_path, file_name, source, destination)
         # Assert
-        with open('build/sample_file_to_pythonify.tmp.py') as stream:
-            content = stream.read()
-            # check that {{ project_name }} is not missing after copied
-            self.assertIn('{{ project_name }}', content)
+        try:
+            with open('build/sample_file_to_pythonify.tmp.py') as stream:
+                content = stream.read()
+                # check that {{ project_name }} is not missing after copied
+                self.assertIn('{{ project_name }}', content)
+        except IOError:
+            self.fail('build/sample_file_to_pythonify.tmp.py not found')
         
+    def test_unpythonify(self):
+        # Arrange
+        self.test_pythonify() # pythonify sample_file_to_pythonify.tmp
+        project_dir = '.'
+        file_name = 'sample_file_to_pythonify.tmp'
+        source = ['build']
+        destination = ['tests']
+        # Act
+        unpythonify(project_dir, file_name, source, destination) 
+        try:
+            with open('tests/sample_file_to_pythonify.tmp') as stream:
+                pass # This should not raise
+        except IOError:
+            self.fail('tests/sample_file_to_pythonify.tmp not found')
+
+    def test_create_project(self):
+        """
+        Scenario:
+        1. pythonify tests/sample_file_to_pythonify (results in 
+           build/sample_file_to_pythonify.tmp.py)
+        2. create project named teddy using the build package as a template
+        Expected:
+        - {{ project_name }} in teddy/sample_file_to_pythonify.tmp.py is 
+          rendered as teddy (due to Django template capability)
+        """
+        self.test_pythonify()
+        create_project('teddy', 'build')
+        with open('teddy/sample_file_to_pythonify.tmp.py') as stream:
+            content = stream.read()
+            self.assertIn('teddy', content)
+            self.assertFalse('{{ project_name }}' in content)
+
+    def test_cleanup(self):
+        create_python_package('sample_project')
+        os.makedirs('sample_project/build')
+        cleanup('sample_project')
+        self.assertFalse(os.path.exists('sample_project/build'))
+        self.assertFalse(os.path.exists('sample_project/__init__.py'))
+
+    def test_chmod_scripts(self):
+        # Arrange
+        os.makedirs('sample_project')
+        call(["touch", 'sample_project/runserver'])
+        call(["touch", 'sample_project/runtests'])
+        call(["touch", 'sample_project/reset_db'])
+        # Act
+        chmod_scripts('sample_project')
+        # Assert
+        owner_can_execute = stat.S_IXUSR
+        st = os.stat('sample_project/runserver')
+        self.assertTrue(st.st_mode & owner_can_execute)
+        st = os.stat('sample_project/runtests')
+        self.assertTrue(st.st_mode & owner_can_execute)
+        st = os.stat('sample_project/reset_db')
+        self.assertTrue(st.st_mode & owner_can_execute)
+        
+
 class TestTransactionMiddleWare(TestCase):
     def test_transaction_middleware_exist(self):
         """
