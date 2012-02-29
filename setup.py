633c45b8ae686aa618f5eeee829136f7831e6c72
juacompe <juacompe@gmail.com>
2012-02-29 22:40:35 +0700

    - add 2 new scripts in setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,24 @@ setup(
     author = 'Proteus Technologies',
     author_email = 'dev-team@proteus-tech.com',
     zip_safe = False, # for project_template
-    scripts = ['bin/startproject'],
+    scripts = ['bin/startproject'
+        , 'bin/start-simple-server'
+        , 'bin/setup-buildbot-on-server'
+    ],
     packages = ['project_template'
         , 'project_template.project_name'
         , 'project_template.project_name.tests'
         , 'project_template.project_name_project'
         , 'project_template.project_name_project.settings'
+        , 'proteus'
         , '.'
     ],
     package_data = {
         'project_template': ['setup/*'
-            ,'buildbot/*'
-            ,'runtests'
-            ,'runserver'
-            ,'reset_db' 
+            , 'buildbot/*'
+            , 'runtests'
+            , 'runserver'
+            , 'reset_db' 
         ],
     },
     install_requires = [
