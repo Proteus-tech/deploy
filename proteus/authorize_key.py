1d6e9d39e38f15288253d9da9f97c8c85846c926
juacompe <juacompe@gmail.com>
2012-03-07 21:03:10 +0700

    - stop adding duplicated key

--- proteus/authorize_key.py
+++ proteus/authorize_key.py
@@ -5,10 +5,12 @@ from fabric.operations import run, sudo
 class Configure(Role):
     def configure(self, server):
         '''
-        add public key from of EC2 host to the remote host 
+        add public key from of EC2 host to the remote host if not exist
         '''
         public_key = sudo("cat /home/www-data/.ssh/id_rsa.pub", user='www-data')
         remote_host = self.parameter 
         with settings(host_string=remote_host):
-            run('echo "%s" >> ~/.ssh/authorized_keys' % public_key)
+            authorized_keys = run('cat ~/.ssh/authorized_keys')
+            if not public_key in authorized_keys:
+                run('echo "%s" >> ~/.ssh/authorized_keys' % public_key)
 
