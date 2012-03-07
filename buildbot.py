c78fbfb1188cf3ba8fb1886de53215a027d494ad
juacompe <juacompe@gmail.com>
2012-03-07 22:06:51 +0700

    - split out settings home folder of www-data into www_home - manual test and setup and start buildbot work

--- buildbot.py
+++ buildbot.py
@@ -1,21 +1,30 @@
 from profab.server import Server
 import simpleserver
 
-def add_buildbot(server, project_name, repository):
+def add_buildbot(server, project_name, repository, privacy):
     role_tuple_list = [
+        ('proteus.www_home',''),
+    ]
+    if privacy == 'private':
+        print 'add ssh key-gen into the roles'
+        role_tuple_list += [
+            ('proteus.ssh_key_gen', ''),
+            ('proteus.authorize_key', repository),
+        ]
+    role_tuple_list += [
         ('proteus.buildbot','%s,%s' % (project_name, repository) ),
     ]
     server.add_roles( server.get_role_adders(*role_tuple_list) )
 
-def setup( using_client, ec2_host, project_name, repository ):
+def setup(using_client, ec2_host, project_name, repository, privacy='public'):
     '''
     Adding buildbot role : using_client, ec2_host, project_name, repository
     '''
     server = Server.connect( client=using_client, hostname=ec2_host )
     if server:
-        add_buildbot(server, project_name, repository)
+        add_buildbot(server, project_name, repository, privacy)
     
-def start( using_client, project_name, repository, *args):
+def start(using_client, project_name, repository, privacy='public', *args):
     server = simpleserver.start(using_client, *args)
-    add_buildbot(server, project_name, repository)
+    add_buildbot(server, project_name, repository, privacy)
 
