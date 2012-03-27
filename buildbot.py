5b0bc5a4bf98bf9abe4eceef159090a396abf025
juacompe <juacompe@gmail.com>
2012-03-27 13:08:30 +0700

    - refactor: rename variables in buildbot.py

--- buildbot.py
+++ buildbot.py
@@ -20,28 +20,29 @@ def add_buildbot(server, project_name, repository, privacy):
             ('proteus.trust_host', host),
         ]
     virtenv_path = virtual_env_path(project_name)
-    buildbot_master_virtenv = '%s-master' % virtenv_path
-    buildbot_slave_virtenv = '%s-slave' % virtenv_path
-    master_checkout_path = "/home/www-data/Buildbot/%s" % (project_name)
+    master_virtenv = '%s-master' % virtenv_path
+    slave_virtenv = '%s-slave' % virtenv_path
+    root = "/home/www-data/Buildbot/%s" % (project_name)
+    master_checkout_path = root 
     master_checkout_parameters = '%s,%s' % (master_checkout_path, repository)
-    buildbot_slave_path = '/home/www-data/Buildbot/%s/buildslave1' % (project_name)
-    slave_checkout_path = "%s/builder-sqlite" % (buildbot_slave_path)
-    buildbot_master_path = '/home/www-data/Buildbot/%s/buildbot-master' % (project_name) 
-    master_cfg_src = '/home/www-data/Buildbot/%s/src/buildbot/master.cfg' % (project_name)
-    master_cfg_dest = '%s/master.cfg' % (buildbot_master_path)
+    slave_path = '%s/buildslave1' % (root)
+    slave_checkout_path = "%s/builder-sqlite" % (slave_path)
+    master_path = '%s/buildbot-master' % (root) 
+    master_cfg_src = '%s/src/buildbot/master.cfg' % (root)
+    master_cfg_dest = '%s/master.cfg' % (master_path)
     complete_params = '%s,%s' % (master_cfg_dest, repository)
     slave_params = '%s,%s,%s' % (project_name,'slave1','localhost')
     role_tuple_list += [
-        ('proteus.install_buildbot_master_env', buildbot_master_virtenv)
+        ('proteus.install_buildbot_master_env', master_virtenv)
         ,('proteus.setup_buildbot_master', project_name)
         ,('proteus.git_checkout', master_checkout_parameters)
         ,('proteus.create_symlink', '%s,%s' % (master_cfg_src, master_cfg_dest))
         ,('proteus.complete_master_config', complete_params)
-        ,('proteus.check_config','%s,%s' % (master_cfg_dest, buildbot_master_virtenv))
-        ,('proteus.install_buildbot_slave_env', buildbot_slave_virtenv)
+        ,('proteus.check_config','%s,%s' % (master_cfg_dest, master_virtenv))
+        ,('proteus.install_buildbot_slave_env', slave_virtenv)
         ,('proteus.setup_buildbot_slave', slave_params)
-        ,('proteus.start_buildbot_master', '%s,%s' % (buildbot_master_path, buildbot_master_virtenv))
-        ,('proteus.start_buildbot_slave', '%s,%s' % (buildbot_slave_path, buildbot_slave_virtenv))
+        ,('proteus.start_buildbot_master', '%s,%s' % (master_path, master_virtenv))
+        ,('proteus.start_buildbot_slave', '%s,%s' % (slave_path, slave_virtenv))
         ,('proteus.buildbot','%s,%s' % (project_name, repository) )
         ,('smarthost',None)
     ]
