88597e10859c9306ed5f5548efed911cd2110096
siraset <siraset@proteus-tech.com>
2012-03-28 00:26:44 +0700

    - added setup-buildbot-slave-on-server - added function to deal with buildbot slave

--- buildbot.py
+++ buildbot.py
@@ -59,6 +59,45 @@ def add_buildbot(server, project_name, repository, privacy):
     ]
     server.add_roles( server.get_role_adders(*role_tuple_list) )
 
+def add_buildbot_slave(server, project_name, ec2_master_host, repository, privacy):
+    role_tuple_list = [
+        ('proteus.www_home',''),
+    ]
+    if privacy == 'private':
+        print 'do hand shaking for private repository'
+        user, host, path = split_private_git_url(repository)
+        role_tuple_list += [
+            ('proteus.ssh_key_gen', ''),
+            ('proteus.authorize_key', repository),
+            ('proteus.trust_host', host),
+        ]
+    root = "/home/www-data/Buildbot/%s" % (project_name)
+    # virtual environment
+    virtenv_path = virtual_env_path(root)
+    slave_virtenv = '%s-slave' % virtenv_path
+    # slave parameters
+    slave_path = '%s/buildslave1' % (root)
+    slave_checkout_path = "%s/builder-sqlite" % (slave_path)
+    slave_checkout_parameters = '%s,%s' % (slave_checkout_path, repository)
+    slave_setup_params = '%s,%s,%s' % (root,'slave1', ec2_master_host)
+
+    role_tuple_list += [
+        ('proteus.install_buildbot_slave_env', slave_virtenv)
+        ,('proteus.tag', 'slave,env-installed')
+        ,('proteus.setup_buildbot_slave', slave_setup_params)
+        ,('proteus.git_checkout', slave_checkout_parameters)
+        ,('proteus.tag', 'slave,ready')
+    ]  
+    server.add_roles( server.get_role_adders(*role_tuple_list) )
+
+def setup_buildbot_slave(using_client, ec2_host, ec2_master_host, project_name, repository, privacy='public'):
+    '''
+    Adding buildbot slave role : using_client, ec2_host, ec2_master_host, project_name, repository
+    '''
+    server = Server.connect( client=using_client, hostname=ec2_host )
+    if server:
+        add_buildbot_slave( server, project_name, ec2_master_host, repository, privacy )
+
 def setup(using_client, ec2_host, project_name, repository, privacy='public'):
     '''
     Adding buildbot role : using_client, ec2_host, project_name, repository
