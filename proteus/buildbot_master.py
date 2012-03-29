a355ad1ed9937b12eeeebebbce1519e0cbdc19cb
juacompe <juacompe@gmail.com>
2012-03-29 17:23:17 +0700

    - add tests for functions that generate paths from buildbot_home

--- proteus/buildbot_master.py
+++ proteus/buildbot_master.py
@@ -1,5 +1,5 @@
 from profab.role import Role
-from proteus.buildbot import virtual_env_path 
+from proteus.buildbot import virtual_env_path, home
 from proteus.check_config import check_config
 from proteus.complete_master_config import complete_master_config 
 from proteus.create_symlink import create_symlink
@@ -7,6 +7,14 @@ from proteus.git_checkout import root_folder, git_checkout
 from proteus.install_buildbot_master_env import install_buildbot_master_env
 from proteus.setup_buildbot_master import setup_buildbot_master
 from proteus.tag import tag
+
+def master_virtual_env_path(root):
+    virtenv_path = virtual_env_path(root)
+    return '%s-master' % virtenv_path
+
+def master_location(root):
+    return '%s/buildbot-master' % (root)
+
 
 class Configure(Role):
     """
@@ -21,17 +29,16 @@ class Configure(Role):
     def configure(self, server):
         repository = self.parameter 
         project_name = root_folder(repository)
-        root = "/home/www-data/Buildbot/%s" % (project_name)
+        root = home(project_name)
  
-        virtenv_path = virtual_env_path(root)
-        master_virtenv = '%s-master' % virtenv_path
+        master_virtenv = master_virtual_env_path(root)
         install_buildbot_master_env(server, master_virtenv)
         tag(server, 'master', 'env-installed')
 
         setup_buildbot_master(server, root) 
         git_checkout(server, root, repository)
 
-        master_path = '%s/buildbot-master' % (root) 
+        master_path = master_location(root) 
         master_cfg_src = '%s/src/buildbot/master.cfg' % (root)
         master_cfg_dest = '%s/master.cfg' % (master_path)
         create_symlink(server, master_cfg_src, master_cfg_dest)
--- proteus/buildbot_slave.py
+++ proteus/buildbot_slave.py
@@ -1,9 +1,17 @@
 from profab.role import Role
-from proteus.buildbot import virtual_env_path 
+from proteus.buildbot import virtual_env_path, home
 from proteus.git_checkout import root_folder, git_checkout
 from proteus.install_buildbot_slave_env import install_buildbot_slave_env
 from proteus.setup_buildbot_slave import setup_buildbot_slave
 from proteus.tag import tag
+
+def slave_virtual_env_path(root):
+    virtenv_path = virtual_env_path(root)
+    return '%s-slave' % virtenv_path
+
+def slave_location(root):
+    return '%s/buildslave1' % (root)
+
 
 class Configure(Role):
     """
@@ -18,16 +26,15 @@ class Configure(Role):
     def configure(self, server):
         repository = self.parameter 
         project_name = root_folder(repository)
-        root = "/home/www-data/Buildbot/%s" % (project_name)
+        root = home(project_name) 
 
-        virtenv_path = virtual_env_path(root)
-        slave_virtenv = '%s-slave' % virtenv_path
+        slave_virtenv = slave_virtual_env_path(root)
         install_buildbot_slave_env(server, slave_virtenv)
         tag(server, 'slave', 'env-installed')
 
         setup_buildbot_slave(server, root, 'slave1', 'localhost')
 
-        slave_path = '%s/buildslave1' % (root)
+        slave_path = slave_location(root)
         slave_checkout_path = "%s/builder-sqlite" % (slave_path)
         git_checkout(server, slave_checkout_path, repository)
         tag(server, 'slave', 'ready')
