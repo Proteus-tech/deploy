5ded32151cc4063353e707c2444b344c66eb37cb
siraset <siraset@proteus-tech.com>
2012-03-29 16:24:54 +0700

    - make setup_library more readable

--- proteus/setup_library.py
+++ proteus/setup_library.py
@@ -1,22 +1,27 @@
-from fabric.context_managers import prefix
-from fabric.operations import run, sudo
+from fabric.operations import sudo
 from fabric.api import cd
+from fabric.contrib.files import exists
 from profab.role import Role
+from profab import _logger
 
 class Configure(Role):
     """
     Install packages from "project_root_path"
+    e.g.
+        project_root_path = '/home/www-data/project-name'
     """
     def configure(self, server):
         project_root_path = self.parameter
         with cd(project_root_path):
-            if exists('setup/require_libs.txt'):
-                list_pkg = run('cat setup/require_libs.txt')
+            if exists('setup/requirelibs.txt'):
+                list_pkg = run('cat setup/requirelibs.txt')
                 # This split should be work for linux
                 pkgs = list_pkg.split('\r\n')
                 for pkg in pkgs:
-                    sudo('apt-get install -y %s' % (pkg))
+                    if pkg != '':
+                        sudo('apt-get install -y %s' % (pkg))
             else:
-                print 'setup/require_libs.txt not found!'
+                _logger.info('setup/requirelibs.txt not found!')
+
 
                      
