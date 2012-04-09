86d563652f949d6a002239eef70ed6624519862e
juacompe <juacompe@gmail.com>
2012-04-09 21:54:46 +0700

    - refactor: rename master_config_file -> config_file   as it has changed to settings.py

--- proteus/complete_master_config.py
+++ proteus/complete_master_config.py
@@ -2,16 +2,16 @@ from fabric.contrib.files import sed
 from profab.role import Role
 from buildbot import splitter
 
-def complete_master_config(server, master_config_file, git_url):
-    sed(master_config_file,'/path/to/repo', git_url, use_sudo=True)
-    sed(master_config_file,'project_dirname', 'src', use_sudo=True)
-    sed(master_config_file,'buildbot_master_host', server.instance.dns_name, use_sudo=True)
+def complete_master_config(server, config_file, git_url):
+    sed(config_file,'/path/to/repo', git_url, use_sudo=True)
+    sed(config_file,'project_dirname', 'src', use_sudo=True)
+    sed(config_file,'buildbot_master_host', server.instance.dns_name, use_sudo=True)
 
 class Configure(Role):
     '''
-    Complete Buildbot Master configuration file with "master_config_full_path,git_url"    
+    Complete Buildbot Master configuration file with "config_full_path,git_url"    
     '''
     def configure(self, server):
-        master_config_file, git_url = splitter(self.parameter)
-        complete_master_config(server, master_config_file, git_url)
+        config_file, git_url = splitter(self.parameter)
+        complete_master_config(server, config_file, git_url)
 
