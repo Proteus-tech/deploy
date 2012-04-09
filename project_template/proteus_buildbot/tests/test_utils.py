d648809f67d80e20f8c94eeda8c6082975b6efb2
juacompe <juacompe@gmail.com>
2012-04-10 02:23:50 +0700

    - ignore __init__ - add module_name (bcoz dir(module) didn't work)

--- project_template/proteus_buildbot/tests/test_utils.py
+++ project_template/proteus_buildbot/tests/test_utils.py
@@ -1,4 +1,4 @@
-from proteus_buildbot.utils import list_modules
+from proteus_buildbot.utils import list_modules, module_name
 from unittest import TestCase
 
 class TestUtils(TestCase):
@@ -6,9 +6,9 @@ class TestUtils(TestCase):
         slaves = __import__('proteus_buildbot.slaves', fromlist=['proteus_buildbot', 'slaves'])
         sub_modules = list_modules(slaves) 
         self.assertTrue('sqlite_slave' in sub_modules)
+        self.assertTrue('__init__' not in sub_modules)
 
     def test_list_modules_with_proteus(self):
-        from proteus import buildbot_master
         proteus = __import__('proteus')
         sub_modules = list_modules(proteus) 
         self.assertTrue('buildbot' in sub_modules)
@@ -19,4 +19,24 @@ class TestUtils(TestCase):
         self.assertTrue('git_checkout' in sub_modules)
         self.assertTrue('setup_buildbot_master' in sub_modules)
         self.assertTrue('tag' in sub_modules)
+
+    def test_module_name_for_dot_py(self):
+        module = 'sqlite_slave.py'
+        result = module_name(module)
+        self.assertEqual('sqlite_slave', result)
+
+    def test_module_name_for_dot_pyc(self):
+        module = 'sqlite_slave.pyc'
+        result = module_name(module)
+        self.assertEqual('sqlite_slave', result)
+
+    def test_module_name_for_dot_swp(self):
+        module = 'sqlite_slave.py.swp'
+        result = module_name(module)
+        self.assertEqual(None, result)
+
+    def test_module_name_for_package(self):
+        module = 'tests'
+        result = module_name(module)
+        self.assertEqual('tests', result)
 
