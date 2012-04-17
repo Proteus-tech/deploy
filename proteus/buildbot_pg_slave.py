02e89c63e2e65a28320e30447f358b429a2ef4c2
siraset <siraset@proteus-tech.com>
2012-04-18 01:41:08 +0700

    added new roles - buildbot_pg_slave - setup_buildbot_pg_slave registered new script to setup.py

--- proteus/buildbot_pg_slave.py
+++ proteus/buildbot_pg_slave.py
@@ -2,7 +2,7 @@ from profab.role import Role
 from proteus.buildbot import virtual_env_path, home, splitter
 from proteus.git_checkout import root_folder, git_checkout
 from proteus.install_buildbot_slave_env import install_buildbot_slave_env
-from proteus.setup_buildbot_slave import setup_buildbot_slave
+from proteus.setup_buildbot_pg_slave import setup_buildbot_pg_slave
 from proteus.tag import tag
 from proteus.setup_library import setup_library
 
@@ -38,7 +38,7 @@ class Configure(Role):
         install_buildbot_slave_env(server, slave_virtenv)
         tag(server, 'slave', 'env-installed')
 
-        setup_buildbot_slave(server, root, 'slave-pg', ec2_master_host)
+        setup_buildbot_pg_slave(server, root, 'slave-pg', ec2_master_host)
 
         slave_path = slave_location(root)
         slave_checkout_path = "%s/builder-pg" % (slave_path)
