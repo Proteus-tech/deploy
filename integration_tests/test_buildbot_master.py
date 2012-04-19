2ebaf313c324264190ff5380f67fa6b367187fe4
juacompe <juacompe@gmail.com>
2012-04-19 12:13:08 +0700

    - remove unused code - setUpClass and tearDownClass works - split server start into a separate function - can test that environment can be activated

--- integration_tests/test_buildbot_master.py
+++ integration_tests/test_buildbot_master.py
@@ -1,12 +1,20 @@
-from profab.server import Server
+from fabric.api import settings
+from fabric.context_managers import prefix
+from fabric.operations import sudo
+from os.path import abspath
+from profab.server import Server, _on_this_server
+from proteus.install_buildbot_master_env import install_buildbot_master_env
 from subprocess import call
 from unittest import TestCase
+import sys
 
-
-   
 class TestBuildbotMaster(TestCase):
     @classmethod
-    def setupClass(cls):
+    def create_simple_server(cls):
+        """
+        Simulate bin/start-simple-server script by create a new instance using
+        roles as simple server script. 
+        """
         role_tuple_list = [
               ('security_group','ssh')
             , ('security_group','http')
@@ -15,21 +23,42 @@ class TestBuildbotMaster(TestCase):
             , ('region','us-west-1')
             , ('size','t1.micro')
             , ('ami','ami-4d580408')
-            #, ('proteus.www_home','')
-            #, ('proteus.install_buildbot_master_env','/home/www-data/Buildbot/fluffy/virtenv-master')                                    
-            #, ('proteus.tag','master,env-installed')
         ]
-        #cls.server = Server.connect('proteus', 'ec2-184-169-248-190.us-west-1.compute.amazonaws.com')
-        cls.server = Server.start('proteus', *role_tuple_list)
+        #cls.server = Server.start('proteus', *role_tuple_list)
+        cls.server = Server.connect('proteus', 'ec2-184-72-4-120.us-west-1.compute.amazonaws.com')
         assert cls.server, 'Server should be connected'
-        #server.add_roles( server.get_role_adders(*role_tuple_list) )
+
+    @classmethod
+    def setUpClass(cls):
+        cls.create_simple_server()
 
     @classmethod
     def tearDownClass(cls):
-        assert cls.server, 'Server should be connected'
-        cls.server.terminate()
+        pass
+        #cls.server.terminate()
+        
+    def delete_buildbot_folder(self):
+        with settings(host_string=host_string):
+            sudo('rm -rf /home/www-data/Buildbot')
 
     def test_setup_buildbot_master_env(self):
-        call(['ls', '-la', '../bin/']) 
-        assert self.server, 'Server should be connected'
+        # Arrange
+        master_virtual_env_path = '/home/www-data/Buildbot/hobby/virtenv-master'
+        # Act
+        # TODO call from script to test script too
+        ec2_host = self.server.instance.dns_name
+        #call(['pf-server-role-add'
+        #    , 'proteus'
+        #    , ec2_host
+        #    , '--proteus.install_buildbot_master_env'
+        #    , master_virtual_env_path]) 
+        remote_function = _on_this_server(install_buildbot_master_env)
+        remote_function(self.server, master_virtual_env_path)
+        # Assert
+        host_string = 'ubuntu@%s' % ec2_host
+        with settings(host_string=host_string):
+            activate = 'source %s/bin/activate' % master_virtual_env_path
+            with prefix(activate):
+                output = sudo('env | grep virtenv-master')
+                self.assertTrue('VIRTUAL_ENV=/home/www-data/Buildbot/hobby/virtenv-master' in output)
 
