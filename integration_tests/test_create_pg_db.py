5229abc829f56924050aad5d9c9773ad20a2482c
AttakornP <cartoonman_jr@hotmail.com>
2012-04-26 17:08:00 +0700

    add integration_test_master_buildbot and fix integration_test_create_pg_db

--- integration_tests/test_create_pg_db.py
+++ integration_tests/test_create_pg_db.py
@@ -1,11 +1,10 @@
-#!usr/bin/python
 from fabric.api import settings
 from fabric.context_managers import prefix
 from fabric.contrib.files import sed
-from fabric.operations import sudo, local, run
+from fabric.operations import sudo, run
 from os.path import abspath
 from profab.server import Server, _on_this_server
-from proteus.install_buildbot_master_env import install_buildbot_master_env
+from proteus.install_buildbot_slave_env import install_buildbot_slave_env
 from subprocess import call
 from unittest import TestCase
 import sys, os
@@ -48,8 +47,8 @@ class TestCreatePgDb(TestCase):
         os.system('pip install nose')
         os.chdir(tmp_path)
        
-        cls.create_simple_server()
-        #cls.connect_to_server('ec2-50-18-15-96.us-west-1.compute.amazonaws.com')
+        #cls.create_simple_server()
+        cls.connect_to_server('ec2-184-72-29-114.us-west-1.compute.amazonaws.com')
 
     @classmethod
     def tearDownClass(cls):
@@ -59,81 +58,97 @@ class TestCreatePgDb(TestCase):
         '''
         os.system('rm -rf /tmp/proteus-deploy-int/')
         os.system('rm -rf ../build/ ../dist/ ../proteus_deploy.egg-info/')
-        cls.server.stop()
-        cls.server.terminate()
+        #cls.server.stop()
+        #cls.server.terminate()
         
     def setUp(self):
         self.ec2_host = self.server.instance.dns_name
         self.host_string = 'ubuntu@%s' % self.ec2_host
         
     def tearDown(self):
-        self.delete_buildbot_folder()
+        #self.delete_buildbot_folder()
+        pass
 
-    def delete_buildbot_folder(self):
-        with settings(host_string=self.host_string):
-            sudo('rm -rf /home/www-data/Buildbot')
+    #def delete_buildbot_folder(self):
+    #    with settings(host_string=self.host_string):
+    #        sudo('rm -rf /home/www-data/Buildbot')
 
     def test_create_pg_db(self):
         # Arrange
         slave_virtual_env_path = '/home/www-data/Buildbot/hobby/virtenv-slave'
+        ''' 1.run role install_buildbot_slave_env
+        '''
         # Act
-        call(['setup-buildbot-on-server'
+        call(['pf-server-role-add'
             , 'proteus'
             , self.ec2_host
-            , 'hobby'
-            , 'git://github.com/juacompe/hobby.git']) 
+            , '--proteus.install_buildbot_slave_env'
+            , slave_virtual_env_path]) 
         # Assert
         with settings(host_string=self.host_string):
-            # test have postgresql in server
-            output = sudo('ls /home/www-data/Buildbot/')
-            self.assertTrue('hobby' in output)
+            # test have slave env slave on server
+            output = sudo('ls /home/www-data/Buildbot/hobby')
+            self.assertTrue('virtenv-slave' in output)
 
+        ''' 2.run role setup_buildbot_pg_slave
+        '''
         # Act
         call(['pf-server-role-add'
             , 'proteus'
             , self.ec2_host
-            , 'postgres']) 
+            , '--proteus.setup_buildbot_pg_slave'
+            , '/home/www-data/Buildbot/hobby,slave-pg,localhost']) 
         # Assert
         with settings(host_string=self.host_string):
-            # test have postgresql in server
-            output = sudo('ls /etc/')
-            self.assertTrue('postgresql' in output)
-        
-        # Act
-        call(['pf-server-role-add'
-            , 'proteus'
-            , self.ec2_host
-            , 'proteus.replace_pg_hba_conf']) 
-        # Assert
-        with settings(host_string=self.host_string):
-            # test pg_hba.conf is modified
-            output = sudo('cat /etc/postgresql/8.4/main/pg_hba.conf')
-            self.assertTrue('local   all all             trust' in output)
-            self.assertTrue('host    all all     127.0.0.1/32    md5' in output)    
-        
-        # Assert
-        with settings(host_string=self.host_string):
-            # test rename folder
-            sudo('mv /home/www-data/Buildbot/hobby/buildslave1/builder-sqlite/'
-                 ' /home/www-data/Buildbot/hobby/buildslave1/builder-pg/')
-            output = sudo('ls /home/www-data/Buildbot/hobby/buildslave1/')
+            # test directory is created
+            output = sudo('ls /home/www-data/Buildbot/hobby/buildslave1')
             self.assertTrue('builder-pg' in output)
-        
-        # Assert
-        with settings(host_string=self.host_string):
-            # test fix file buildbot.tac
-            sed('/home/www-data/Buildbot/hobby/buildslave1/buildbot.tac'
-                , "slave-sqlite"
-                , "slave-pg"
-                , use_sudo=True)
-            sed('/home/www-data/Buildbot/hobby/buildslave1/buildbot.tac'
-                , "slave-sqlitepassword"
-                , "slave-pgpassword"
-                , use_sudo=True)
+            # test file is changed
             output = sudo('cat /home/www-data/Buildbot/hobby/buildslave1/buildbot.tac')
             self.assertTrue("slavename = 'slave-pg'" in output)
             self.assertTrue("passwd = 'slave-pgpassword'" in output)
         
+        ''' 3.run role git_checkout
+        '''
+        # Act
+        call(['pf-server-role-add'
+            , 'proteus'
+            , self.ec2_host
+            , '--proteus.git_checkout'
+            , '/home/www-data/Buildbot/hobby/buildslave1/builder-pg,git://github.com/juacompe/hobby.git']) 
+        # Assert
+        with settings(host_string=self.host_string):
+            # test directory is created
+            output = sudo('ls /home/www-data/Buildbot/hobby/buildslave1/builder-pg')
+            self.assertTrue('src' in output)
+            output = sudo('ls /home/www-data/Buildbot/hobby/buildslave1/builder-pg/bin')
+            self.assertTrue('update_master_config' in output)
+        
+        ''' 4.run role setup_library
+        '''
+        call(['pf-server-role-add'
+            , 'proteus'
+            , self.ec2_host
+            , '--proteus.setup_library'
+            , '/home/www-data/Buildbot/hobby/buildslave1/builder-pg/src/setup/requirelibs.txt']) 
+        # Assert
+        with settings(host_string=self.host_string):
+            # test package is installed
+            output = sudo('aptitude search libpq-dev')
+            self.assertTrue('i   libpq-dev' in output)
+            output = sudo('aptitude search build-essential')
+            self.assertTrue('i   build-essential' in output)
+            output = sudo('aptitude search libpng-dev')
+            self.assertTrue('v   libpng-dev' in output)
+            output = sudo('aptitude search libjpeg-dev')
+            self.assertTrue('v   libjpeg-dev' in output)
+            output = sudo('aptitude search python-dev')
+            self.assertTrue('i   python-dev' in output)
+            output = sudo('aptitude search python-setuptools')
+            self.assertTrue('i   python-setuptools' in output)
+
+        ''' 5.run role setup_psycopg2_on_slave
+        '''
         # Act
         call(['pf-server-role-add'
             , 'proteus'
@@ -148,20 +163,41 @@ class TestCreatePgDb(TestCase):
                 output = sudo('pip freeze')
                 self.assertTrue('psycopg2' in output)
         
+        '''6.run role postgres
+        '''
         # Act
-        call(['restart-buildbot-master'
+        call(['pf-server-role-add'
             , 'proteus'
             , self.ec2_host
-            , 'hobby'])      
-        call(['restart-buildbot-slave'
+            , 'postgres']) 
+        # Assert
+        with settings(host_string=self.host_string):
+            # test have postgresql in server
+            output = sudo('ls /etc/')
+            self.assertTrue('postgresql' in output)
+        
+        '''7.run role replace_pg_hba_conf
+        '''
+        # Act
+        call(['pf-server-role-add'
             , 'proteus'
             , self.ec2_host
-            , 'hobby']) 
+            , 'proteus.replace_pg_hba_conf']) 
+        # Assert
+        with settings(host_string=self.host_string):
+            # test pg_hba.conf is modified
+            output = sudo('cat /etc/postgresql/8.4/main/pg_hba.conf')
+            self.assertTrue('local   all all             trust' in output)
+            self.assertTrue('host    all all     127.0.0.1/32    md5' in output)    
+        
+        ''' 8. run role create_pg_db'
+        '''
+        # Act
         call(['pf-server-role-add'
             , 'proteus'
             , self.ec2_host
             , '--proteus.create_pg_db'
-            , 'hobby']) 
+            , 'hobby,git://github.com/juacompe/hobby.git']) 
         # Assert
         with settings(host_string=self.host_string):
             # test user is created
@@ -169,3 +205,4 @@ class TestCreatePgDb(TestCase):
             self.assertTrue('hobbyuser' in output)
             # test database is created
             self.assertTrue('hobby' in output)
+
