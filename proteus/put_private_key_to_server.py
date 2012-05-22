b59dd259ba9a8cbaed52972e38800fea0a5f10ce
AttakornP <cartoonman_jr@hotmail.com>
2012-05-22 20:33:05 +0700

    fix code

--- proteus/put_private_key_to_server.py
+++ proteus/put_private_key_to_server.py
@@ -3,17 +3,16 @@ from fabric.contrib.files import exists
 from fabric.operations import run, sudo
 from profab.role import Role
 from proteus.authorize_key import authorize_key
-from proteus import buildbot
 
-def put_private_key_to_server(server, current_user, remote_host):
+def put_private_key_to_server(server, current_user):
     current_home = '/home/%s' % current_user
-    private_key = local('sudo cat %s/.ssh/id_rsa' %current_home)
-    with settings(host_string=remote_host):
-        if not exists('%s/.ssh/' % current_home):
-            sudo('mkdir %s/.ssh/' % current_home, user=current_user)
-        if not exists('%s/.ssh/id_rsa' % current_home):
-            sudo('echo "%s" >> %s/.ssh/id_rsa' % (private_key, current_home), user=current_user)
-            sudo('chmod 600 %s/.ssh/id_rsa' % current_home, user=current_user)
+    private_key = local('cat ~/.ssh/id_rsa' %current_home)
+    
+    if not exists('%s/.ssh/' % current_home):
+        sudo('mkdir %s/.ssh/' % current_home, user=current_user)
+    if not exists('%s/.ssh/id_rsa' % current_home):
+        sudo('echo "%s" >> %s/.ssh/id_rsa' % (private_key, current_home), user=current_user)
+        sudo('chmod 600 %s/.ssh/id_rsa' % current_home, user=current_user)
 
 
 class Configure(Role):
@@ -21,11 +20,10 @@ class Configure(Role):
     Role to put private key to target server
     """
     def configure(self, server):
-        if ',' not in self.parameter:
-            remote_host = self.parameter
+        if self.parameter == '':
             current_user = 'www-data'
         else:
-            remote_host, current_user = buildbot.splitter(self.parameter)
+            current_user = self.parameter
         
-        put_private_key_to_server(server, current_user, remote_host)
+        put_private_key_to_server(server, current_user)
 
