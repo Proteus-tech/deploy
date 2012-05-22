a2b02828a700a20719dfd4eaf3fc386e67aba413
ccc <ccc@ccc-vir.(none)>
2012-05-22 17:00:31 +0700

    move create_build_slave_layout to buildbot.py

--- proteus/git_build_checkout.py
+++ proteus/git_build_checkout.py
@@ -2,7 +2,7 @@ from fabric.context_managers import cd
 from profab.role import Role
 from fabric.operations import sudo
 from fabric.contrib.files import exists
-from proteus import git_checkout
+from proteus import git_checkout, buildbot
 
 def git_build_checkout(server, git_url, path):
     project_name = git_checkout.root_folder(git_url)
@@ -15,28 +15,6 @@ def git_build_checkout(server, git_url, path):
             sudo("git pull origin develop", user="www-data")
             sudo("git checkout develop", user="www-data")
 
-def create_build_slave_layout(server, project_name):
-    base_folder = '/home/www-data'
-    with cd(base_folder):
-        current_folder = '%s/current' % (project_name)
-        config_folder = '%s/config' % (project_name)
-        logs_folder = '%s/logs' % (project_name)
-        bin_folder = '%s/bin' % (project_name)
-
-        sudo('mkdir -p %s' % (current_folder), user="www-data") 
-        sudo('mkdir -p %s' % (config_folder), user="www-data")
-        sudo('mkdir -p %s' % (logs_folder), user="www-data")
-        sudo('mkdir -p %s' % (bin_folder), user="www-data")
-
-        sudo('mkdir -p %s/service' % (current_folder), user="www-data")
-        sudo('mkdir -p %s/virtualenv' % (current_folder), user="www-data")
-        sudo('mkdir -p %s/static' % (current_folder), user="www-data")
-
-        buildbot_folder = '%s/buildbot' % (project_name)
-        sudo('mkdir -p %s' % (buildbot_folder), user="www-data")
-        sudo('mkdir -p %s/master' % (buildbot_folder), user="www-data")
-        sudo('mkdir -p %s/slave1' % (buildbot_folder), user="www-data")
-
 class Configure(Role):
     """ 
     Checkout code from git_url to a path with parameters (path, git_url)
@@ -45,7 +23,7 @@ class Configure(Role):
     def configure(self, server):
         git_url = self.parameter
         project_name = git_checkout.root_folder(git_url)
-        create_build_slave_layout(server, project_name)
+        buildbot.create_build_slave_layout(server, project_name)
         path = "/home/www-data/%s" % project_name
         git_build_checkout(server, git_url, path)
 
