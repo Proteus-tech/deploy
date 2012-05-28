5ec07b46a974cef441d4cb81ed629ef3e48e5c7a
siraset <siraset@proteus-tech.com>
2012-05-28 22:46:41 +0700

    clean code for build server script

--- proteus/create_build_package.py
+++ proteus/create_build_package.py
@@ -1,44 +1,70 @@
 #!/usr/bin/env python
+
 from profab.role import Role
-#from profab.state import env
-#from fabric.decorators import with_settings
 from proteus import buildbot
 from fabric.api import local, run
-from proteus import upload_tar_to_s3
+#from proteus import upload_package_to_s3
 
-# should relocate to buildbot.py
 def create_virtenv(server, project_name):
-    with cd(project_name):
-        sudo("scripts/virtenv", user="www-data")
+    deploy_base = "/home/www-data/%s/deploy" % (project_name)
+    project_base = "/home/www-data/%s" % (project_name)
+    with cd(project_base):
+        sudo("virtualenv --no-site-packages virtenv", user="www-data")
+        with prefix("source virtenv/bin/activate"):
+            sudo("pip install -r current/service/setup/requirements.txt", user="www-data")
+            sudo("pip install -r current/service/setup/server.pip", user="www-data")
+            sudo("virtualenv --relocatable virtenv", user="www-data")    
 
-def get_tar_name(server, project_name, svn_rev):
+def get_machine_spec(server):
     ubuntu_version = run('lsb_release', '-cs').strip()
     bits = run('uname', '-m').strip()
-    tarfile = "%s.%s.%s.%s.tar.bz2" % (project_name, svn_rev, ubuntu_version, bits)
-    return tarfile    
+    return (ubuntu_version, bits)
 
 def export_code_from_svn(server, svn_url):
     svn_out = sudo("""svn log -l 1 --non-interactive --no-auth-cache
         --trust-server-cert --username www-data --password 'www-d@t@!@#' %s
         """ % (svn_url), user="www-data")
     svn_rev = long(svn_out.split('\n')[1].split(' ')[0][1:])
-    
-    return get_tar_name(server, project_name, svn_url)
+    ubuntu_version, bits = get_machine_spec(server)
+    return "%s.%s.%s.%s.tar.bz2" % (project_name, svn_rev, ubuntu_version, bits)
 
-def create_build_package(server):
-    '''
-    1. create virtual env
-    2. pip install python lib from requirements.txt
-    3. collectstatic
-    4. runtest
-    5. remove pyc
-    6. tar create tar
-    7. rename tar file
-    '''
+def collect_static(server, project_name):
+    project_base = "/home/www-data/%s" % (project_name)
+    log_folder = "%s/logs" % (project_base)
+    with cd(project_base):
+        sudo("mkdir -p logs",user="www-data")
+        with prefix("source virtenv/bin/activate"):
+            sudo("service/manage.py collectstatic --noinput", user="www-data") 
+
+def create_tar_file(server, project_name, tar_filename):
+    project_base = "/home/www-data/%s" % (project_name)
+    current_revision_project = "%s/current" % (project_name)
+    with cd(current_revision_project):
+        sudo("tar cfa %s service static virtenv" % (
+
+def upload_tar_file(server, client_name, project_name):
     pass
-    
+     
 
 class Configure(Role):
+    """
+    Prerequesties - code should be in working directory.
+    """
     def configure(self, server):
-        pass
+        # check out deploy project to build server
+        build_base = "/home/www-data/build/deploy"
+        git_url = "%s@develop" % ("git://github.com/Proteus-tech/deploy.git")
+        git_checkout(server, build_base, git_url)
 
+        # derive tar file from svn url
+        tar_filename = export_code_from_svn(server,'project_url')
+        
+        # create virtual environment from project name
+        create_virtenv(server,'project_name')
+
+        collect_static(server,'project_name')
+
+        #create_tar_file(server,'project_name')
+
+        #upload_package_to_s3.upload_package_to_s3(server, client_name, project_name)
+        
