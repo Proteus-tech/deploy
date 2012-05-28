f3d3783c10bf7b5de0c0af85d78b64265ee58e87
siraset <siraset@proteus-tech.com>
2012-05-29 00:01:08 +0700

    comment unneeded function for now

--- proteus/create_build_package.py
+++ proteus/create_build_package.py
@@ -27,35 +27,35 @@ def create_virtenv(server, project_name):
             sudo("pip install -r current/service/setup/server.pip", user="www-data")
             sudo("virtualenv --relocatable virtenv", user="www-data")    
 
-def get_machine_spec(server):
-    ubuntu_version = run('lsb_release', '-cs').strip()
-    bits = run('uname', '-m').strip()
-    return (ubuntu_version, bits)
-
-def export_code_from_svn(server, svn_url):
-    svn_out = sudo("""svn log -l 1 --non-interactive --no-auth-cache
-        --trust-server-cert --username www-data --password 'www-d@t@!@#' %s
-        """ % (svn_url), user="www-data")
-    svn_rev = long(svn_out.split('\n')[1].split(' ')[0][1:])
-    ubuntu_version, bits = get_machine_spec(server)
-    return "%s.%s.%s.%s.tar.bz2" % (project_name, svn_rev, ubuntu_version, bits)
-
-def collect_static(server, project_name):
-    project_base = "/home/www-data/%s" % (project_name)
-    log_folder = "%s/logs" % (project_base)
-    with cd(project_base):
-        sudo("mkdir -p logs",user="www-data")
-        with prefix("source virtenv/bin/activate"):
-            sudo("service/manage.py collectstatic --noinput", user="www-data") 
-
-def create_tar_file(server, project_name, tar_filename):
-    project_base = "/home/www-data/%s" % (project_name)
-    current_revision_project = "%s/current" % (project_name)
-    with cd(current_revision_project):
-        sudo("tar cfa %s service static virtenv" % (tar_filename), user='www-data')
-
-def upload_tar_file(server, client_name, project_name):
-    pass
+#def get_machine_spec(server):
+#    ubuntu_version = run('lsb_release', '-cs').strip()
+#    bits = run('uname', '-m').strip()
+#    return (ubuntu_version, bits)
+#
+#def export_code_from_svn(server, svn_url):
+#    svn_out = sudo("""svn log -l 1 --non-interactive --no-auth-cache
+#        --trust-server-cert --username www-data --password 'www-d@t@!@#' %s
+#        """ % (svn_url), user="www-data")
+#    svn_rev = long(svn_out.split('\n')[1].split(' ')[0][1:])
+#    ubuntu_version, bits = get_machine_spec(server)
+#    return "%s.%s.%s.%s.tar.bz2" % (project_name, svn_rev, ubuntu_version, bits)
+#
+#def collect_static(server, project_name):
+#    project_base = "/home/www-data/%s" % (project_name)
+#    log_folder = "%s/logs" % (project_base)
+#    with cd(project_base):
+#        sudo("mkdir -p logs",user="www-data")
+#        with prefix("source virtenv/bin/activate"):
+#            sudo("service/manage.py collectstatic --noinput", user="www-data") 
+#
+#def create_tar_file(server, project_name, tar_filename):
+#    project_base = "/home/www-data/%s" % (project_name)
+#    current_revision_project = "%s/current" % (project_name)
+#    with cd(current_revision_project):
+#        sudo("tar cfa %s service static virtenv" % (tar_filename), user='www-data')
+#
+#def upload_tar_file(server, client_name, project_name):
+#    pass
      
 
 class Configure(Role):
@@ -66,16 +66,6 @@ class Configure(Role):
         svn_url = self.parameter
         project_name = svn_checkout.root_folder(svn_url)
         git_url = "git://github.com/Proteus-tech/deploy.git"
+
         checkout_deploy_code(server, project_name, git_url, branch="feature/PINFR241_siraset") 
-
-        # derive tar file from svn url
-        tar_filename = export_code_from_svn(server, svn_url)
-        
-        # create virtual environment from project name
         create_virtenv(server, project_name)
-        collect_static(server, project_name)
-
-        #create_tar_file(server,'project_name')
-
-        #upload_package_to_s3.upload_package_to_s3(server, client_name, project_name)
-        
