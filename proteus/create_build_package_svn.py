2b8b9864632a00dd7d98c6473d6be4095ccd2c97
siraset <siraset@proteus-tech.com>
2012-06-20 21:28:14 +0700

    chown of build_settings.py

--- proteus/create_build_package_svn.py
+++ proteus/create_build_package_svn.py
@@ -79,6 +79,8 @@ def collect_static(server, project_name):
     service_base_dir = "%s/service" % (current_base_dir)
     project_settings_dir = '%s_project' % (project_name)
     upload_template("utilities/build_settings.py", destination=service_base_dir,context={'project_name':project_settings_dir})
+    with cd(service_base_dir):
+        sudo("chown www-data:www-data build_settings.py")
     with cd(current_base_dir):
         with prefix("source virtualenv/bin/activate"):
             sudo("python service/manage.py collectstatic --noinput --settings=build_settings", user="www-data")
