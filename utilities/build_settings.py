a74babb76fe11d8b7aaae38b02eb2522798fea57
siraset <siraset@proteus-tech.com>
2012-06-20 20:38:33 +0700

    add specifig function

--- utilities/build_settings.py
+++ utilities/build_settings.py
@@ -1,4 +1,14 @@
 from settings import *
+import os
+try:
+    from settings import PROJECT_DIR
+except ImportError:
+    PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    from settings import PROJECT_PATH_JOIN
+except ImportError:
+    PROJECT_PATH_JOIN = lambda a, *p: os.path.join(PROJECT_DIR, a, *p)
 
 # This file is use for point collectstatic to
 #
