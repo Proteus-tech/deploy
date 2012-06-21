256a897aa5af474923015c27b0da23bab5730712
siraset <siraset@proteus-tech.com>
2012-06-21 16:35:22 +0700

    saparate general function to center file

--- proteus/create_build_package_svn.py
+++ proteus/create_build_package_svn.py
@@ -1,48 +1,15 @@
 #!/usr/bin/env python
 
-import os
 
-from fabric.contrib.files import upload_template, exists
-from profab.role import Role
-from proteus import buildbot
-from fabric.context_managers import cd, prefix
-from fabric.api import local, run, sudo
-from profab import _logger
+from fabric.context_managers import cd
+from fabric.api import sudo
+
+from proteus import buildbot as buildbot_utils
+from proteus import buildbot_build_utils as build_utils
 from proteus import svn_checkout
 
-def checkout_deploy_sourcecode(server, project_name, deploy_url, branch="develop"):
-    _logger.info('checkout deploy sourcecode on remote machine')
-    project_base_folder = "/home/www-data/%s" % (project_name)
-    deploy_base_folder = "deploy"
-    with cd(project_base_folder):
-        if not exists(deploy_base_folder):
-            sudo("git clone -q %s %s" % (deploy_url, deploy_base_folder), user="www-data")
-            with cd(deploy_base_folder):
-                sudo("git checkout -b %s" % (branch), user="www-data")
-                sudo("git pull origin %s" % (branch), user="www-data")
-                sudo("git checkout %s" % (branch), user="www-data")
-
-# For PINFR260
-def checkout_deploy_sourcecode_on_local(server, project_name, deploy_git_url, branch="develop"):
-    _logger.info('checkout deploy sourcecode on local machine')
-    project_base_folder = "/home/www-data/Buildbot/%s" % (project_name)
-    deploy_base_folder = '%s/deploy' % (project_base_folder)
-    if not os.path.exists(deploy_base_folder):
-        local("git clone -q %s %s" % (deploy_git_url,deploy_base_folder ))
-        local("cd %s && git checkout -b %s" % (deploy_base_folder, branch))
-        local("cd %s && git pull origin %s" % (deploy_base_folder, branch))
-        local("cd %s && git checkout %s" % (deploy_base_folder, branch))
-
-def create_virtenv(server, project_name):
-    sudo("easy_install virtualenv")
-    project_base_dir = "/home/www-data/%s" % (project_name)
-    current_dir = "%s/current" % (project_base_dir)
-    with cd(current_dir):
-        sudo("virtualenv --no-site-packages virtualenv", user="www-data")
-        with prefix("source virtualenv/bin/activate"):
-            sudo("pip install -r service/setup/requirements.txt", user="www-data")
-            #sudo("pip install -r current/service/setup/server.pip", user="www-data")
-            sudo("virtualenv --relocatable virtualenv", user="www-data")    
+from profab.role import Role
+from profab import _logger
 
 def _get_project_name(svn_url):
     project_name = None
@@ -51,11 +18,6 @@ def _get_project_name(svn_url):
     else:
         project_name = svn_url.split('/')[-2]
     return project_name
-
-def _get_machine_spec(server):
-    ubuntu_version = run('lsb_release -cs').strip()
-    bits = run('uname -m').strip()
-    return (ubuntu_version, bits)
 
 def get_build_package_name(server, svn_url):
     project_name = _get_project_name(svn_url)
@@ -69,30 +31,14 @@ def get_build_package_name(server, svn_url):
             "--username www-data --password 'www-d@t@!@#' %s "
             % (svn_url), user="www-data")
         svn_rev = long(svn_out.split('\n')[1].split(' ')[0][1:])
-        ubuntu_version, bits = _get_machine_spec(server)
+        ubuntu_version, bits = build_utils.get_machine_spec(server)
 
         return "%s.%s.%s.%s.tar.bz2" % (project_name, svn_rev, ubuntu_version, bits)
 
-
-def collect_static(server, project_name):
-    project_base_dir = "/home/www-data/%s" % (project_name)
-    current_base_dir = "%s/current" % (project_base_dir)
-    service_base_dir = "%s/service" % (current_base_dir)
-    project_settings_dir = '%s_project' % (project_name)
-    upload_template("utilities/build_settings.py", destination=service_base_dir,context={'project_name':project_settings_dir}, use_sudo=True)
-    with cd(service_base_dir):
-        sudo("chown www-data:www-data build_settings.py")
-    with cd(current_base_dir):
-        with prefix("source virtualenv/bin/activate"):
-            sudo("python service/manage.py collectstatic --noinput --settings=build_settings", user="www-data")
-
-def create_tar_file(server, project_name, tarfile_name):
-    project_base_dir = "/home/www-data/%s" % (project_name)
-    current_dir = "%s/current" % (project_base_dir)
-    with cd(current_dir):
-        sudo("tar cfa %s service static virtualenv" % (tarfile_name), user='www-data')
-        path_to_tarfile = '%s/%s' % (current_dir, tarfile_name)
-        return path_to_tarfile
+def remove_svn_hidden_folder(server, project_name):
+    code_path = '/home/www-data/%s/current/service' % (project_name)
+    with cd(code_path):
+        sudo("find -name '.svn' | xargs rm -rf", user="www-data")
 
 class Configure(Role):
     """
@@ -109,29 +55,29 @@ class Configure(Role):
         'subversion',
     ]
     def configure(self, server):
-        git_url, svn_url = buildbot.splitter(self.parameter)
+        git_url, svn_url = buildbot_utils.splitter(self.parameter)
 
         # get project name from svn_url
         project_name = svn_checkout.root_folder(svn_url)
 
         # checkout deploy code to use in host machine
         # for using role
-        checkout_deploy_sourcecode(server, project_name, git_url)
+        build_utils.checkout_deploy_sourcecode(server, project_name, git_url)
 
-        # For PINFR260
-        checkout_deploy_sourcecode_on_local(server, project_name, git_url)
+        # clean svn hidden folder of project dir
+        remove_svn_hidden_folder(server, project_name)
 
         # create virtual environment from requirements.txt
-        create_virtenv(server, project_name)
+        build_utils.create_virtenv(server, project_name)
 
         # do collectstatic
-        collect_static(server, project_name)
+        build_utils.collect_static(server, project_name)
 
         # just get only build package name
         bpkg_name = get_build_package_name(server, svn_url)
 
         # create tar file
-        path_to_tarfile = create_tar_file(server, project_name, bpkg_name)
+        path_to_tarfile = build_utils.create_tar_file(server, project_name, bpkg_name)
         _logger.info("%s was created", path_to_tarfile)
 
 
