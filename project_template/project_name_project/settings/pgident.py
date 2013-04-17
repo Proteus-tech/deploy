0c501075d922536e4b20bebb26b6009cdfb6850c
Kirit Sælensminde <k@kirit.com>
2013-04-17 20:16:41 +0700

    Added pgident to the project template.

---
+++ project_template/project_name_project/settings/pgident.py
@@ -0,0 +1,12 @@
+from {{ project_name }}_project.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '{{ project_name }}',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
